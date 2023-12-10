71b0fdea03ee7914038c3c1dd181d8bc42f67545
gwigzz <grdevphp@gmail.com>
2023-12-10 03:31:23 +0100

    v0.3

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import filedialog, messagebox
 
-APP_VERSION     = "0.2"
+APP_VERSION     = "0.3"
 
 WIN_WIDTH       = 850
 WIN_HEIGHT      = 600
 
 BG_COLOR        = "#008EC9"
+
+def open_resize_directory():
+    current_directory = os.getcwd()
+    resize_directory = os.path.join(current_directory, f"resize")
+
+    # Check if directory 'resize' exist
+    if os.path.exists(resize_directory):
+        os.startfile(resize_directory)
+        # messagebox.showinfo("Directory", f"The directory: {resize_directory}")
+        messagebox.showinfo("Open Dir", "The directory has been opened")
+    else:
+        messagebox.showerror("Error", "The folder 'resize' cannot be found")
 
 
 class ImageResizer:
@@ -31,36 +43,42 @@ class ImageResizer:
 
     def create_widgets(self):
         # Label for display image
-        self.image_label = tk.Label(self.win, text=f"Resize image V {APP_VERSION}", background=BG_COLOR, font=('Helvetica', 25), fg="#fff")
+        self.image_label = tk.Label(self.win, text=f"Resize image", background=BG_COLOR, font=('Helvetica', 25), fg="#fff")
         self.image_label.pack(pady=10)
 
         # Btn for select image
-        select_button = tk.Button(self.win, text="Sélectionner une image", command=self.load_image)
+        select_button = tk.Button(self.win, text="Select an image", command=self.load_image)
         select_button.pack(pady=10)
 
+
+
         # Dimension Frame label/entry for W & H
-        dimension_frame = tk.Frame(self.win, background="orange")
+        dimension_frame = tk.Frame(self.win, background=BG_COLOR)
         dimension_frame.pack(side="top", padx=5, pady=10)
 
-        width_label = tk.Label(dimension_frame, text="Largeur:", background=BG_COLOR, fg="#fff")
-        width_label.pack(side="left")
+        width_label = tk.Label(dimension_frame, text="Width:", background=BG_COLOR, fg="#fff")
+        width_label.pack(side="left", padx=3)
         width_entry = tk.Entry(dimension_frame, textvariable=self.width_var, width=6)
-        width_entry.pack(side="left")
+        width_entry.pack(side="left", padx=3)
 
-        height_label = tk.Label(dimension_frame, text="Hauteur:", background=BG_COLOR, fg="#fff")
-        height_label.pack(side="left")
+        height_label = tk.Label(dimension_frame, text="Height:", background=BG_COLOR, fg="#fff")
+        height_label.pack(side="left", padx=3)
         height_entry = tk.Entry(dimension_frame, textvariable=self.height_var, width=6)
-        height_entry.pack(side="left")
+        height_entry.pack(side="left", padx=3)
+
+
 
         # new image name (not required)
-        image_name_label = tk.Label(self.win, text="Nom image (optionnel):", background=BG_COLOR, fg="#fff")
+        image_name_label = tk.Label(self.win, text="Image name (optional):", background=BG_COLOR, fg="#fff")
         image_name_label.pack()
         image_name_entry = tk.Entry(self.win, textvariable=self.image_name_var)
         image_name_entry.pack()
 
+
+
         # Btn resizing img
-        resize_button = tk.Button(self.win, text="Redimensionner", command=self.resize_image)
-        resize_button.pack(pady=10)
+        resize_button = tk.Button(self.win, text="Resize image", command=self.resize_image)
+        resize_button.pack(pady=30, padx=5)
 
     def load_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Images", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
@@ -80,7 +98,7 @@ class ImageResizer:
         try:
 
             if not hasattr(self, 'image'):
-                messagebox.showerror("Erreur", "Veuillez séléctionner une image")
+                messagebox.showerror("Error", "Please select an image")
                 return
 
             width = int(self.width_var.get())
@@ -106,9 +124,9 @@ class ImageResizer:
             save_path = os.path.join(output_folder, f"{user_image_name}{original_extension}") 
             resized_image.save(save_path)
 
-            messagebox.showinfo("Terminé", "L'image a été redimensionnée avec succès et enregistrée.")
+            messagebox.showinfo("Completed", "The image has been successfully resized and saved.")
         except ValueError:
-            messagebox.showerror("Erreur", "Veuillez entrer des dimensions valides.")
+            messagebox.showerror("Error", "Please enter valid dimensions.")
 
 
 
@@ -121,7 +139,7 @@ if __name__ == "__main__":
 
    
     """
-    Centre la fenêtre
+    Center Window
     """
     screen_x = int(win.winfo_screenwidth())
     screen_y = int(win.winfo_screenheight())
@@ -135,22 +153,21 @@ if __name__ == "__main__":
     mainMenu = tk.Menu(win)
 
     first_menu = tk.Menu(mainMenu, tearoff=0)
-    first_menu.add_command(label="Quitter", command=win.quit)
+    first_menu.add_command(label="View Images", command=open_resize_directory)
+    first_menu.add_command(label="Exit", command=win.quit)
 
     second_menu = tk.Menu(mainMenu, tearoff=0)
-    second_menu.add_command(label="Help", state="disabled")
+    second_menu.add_command(label="Help", state="normal")
+    second_menu.add_command(label=f"Version: {APP_VERSION }", state="disabled", underline=1)
 
     mainMenu.add_cascade(label="Menu", menu=first_menu)
     mainMenu.add_cascade(label="About", menu=second_menu)
 
 
-    # btn qui
-    btn_quit = tk.Button(win, text="Quitter", command=win.quit)
+    # btn bottom quit app
+    btn_quit = tk.Button(win, text="Exit", command=win.quit)
     btn_quit.pack(pady=20, padx=20, side="right", anchor="se")  
 
-
-
-
-    win.config(background=BG_COLOR, menu=mainMenu)
+    win.config(bg=BG_COLOR, menu=mainMenu)
 
     win.mainloop()
